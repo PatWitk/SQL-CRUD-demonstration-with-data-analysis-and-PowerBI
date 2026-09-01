d38638a244ad3bec1b1f53790b5fec381d878bff
Pathats <pzwitkowski@gmail.com>
2026-09-01 18:03:33 +0100

    initial commit: SQL CRUD portfolio

---
+++ SQL data analysis.sql
@@ -0,0 +1,27 @@
+/*--- MOST INTERESTING PART OF THE CODE ---*/
+WITH customer_order_summary AS (
+    SELECT orders.customer_id, 
+        COUNT(orders.order_id) AS orders_made,
+        SUM(orders.quantity*product_price.unit_price) AS
+        total_spent
+    FROM orders
+    JOIN product_price ON 
+        (orders.product_id, orders.branch_id) =
+        (product_price.product_id, product_price.branch_id)
+    GROUP BY orders.customer_id
+    HAVING COUNT(order_id) > 3
+    )
+
+SELECT customers.username,customer_order_summary.orders_made,
+    customer_order_summary.total_spent,
+    CASE
+        WHEN customer_order_summary.total_spent > 136000 THEN 'High value'
+        ELSE 'Medium value'
+    END AS customer_tier
+FROM customer_order_summary
+JOIN customers ON customers.customer_id = customer_order_summary.customer_id
+ORDER BY customer_order_summary.total_spent DESC;
+/* In this SELECT sequence I use a CTE to make a temporary table using SUM and COUNT to get sales information on different customers.
+Prices of products vary per branch so our CTE is JOINed to the product_price table which takes a composite key of product_id and branch_id.
+Once we have our CTE made we SELECT the information from it and use a CASE WHEN function to label customer loyalty based on spending habits.
+WORTH NOTING, that prices/expenses here are shown in pence not pounds. */
